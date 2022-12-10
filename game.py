63b257b649884547cf27adf8c305dc53f67437ea
AkritW <mail2max.w@gmail.com>
2022-12-10 15:25:02 +0700

    add support for  horizontal and vertical placement

--- game.py
+++ game.py
@@ -33,18 +33,105 @@ class Reversi:
         self.board = new_board
 
     def get_valid_position(self, player):
-        if player == player.USER.value:
+        if player == Player.USER.value:
             pass
         else:
             pass
+
+    def place(self, player, position):
+        y, x = position
+        self.board[y][x] = 1 if player == Player.USER.value else -1
+
+        if player != self.player:
+            return "It is not your turn!"
+
+        # horizontally check left to right
+        for i in range(1, 8, 1):
+            if player == Player.USER.value:
+                if self.board[y, x + i] == 0:
+                    break
+                elif self.board[y, x + i] == 1:
+                    self.board[y, x : x + i] = [1 for _ in range(i)]
+                elif self.board[y, x + 1] == -1:
+                    pass
+            elif player == Player.AI.value:
+                if self.board[y, x + i] == 0:
+                    break
+                elif self.board[y, x + i] == -1:
+                    self.board[y, x : x + i] = [-1 for _ in range(i)]
+                elif self.board[y, x + 1] == 1:
+                    pass
+
+        # horizontal check right to left
+        for i in range(1, 8, 1):
+            if player == Player.USER.value:
+                if self.board[y, x - i] == 0:
+                    break
+                elif self.board[y, x - i] == 1:
+                    self.board[y, x - i : x] = [1 for _ in range(i)]
+                elif self.board[y, x - i] == -1:
+                    pass
+            elif player == Player.AI.value:
+                if self.board[y][x - i] == 0:
+                    break
+                elif self.board[y, x - i] == -1:
+                    self.board[y, x - i : x] = [-1 for _ in range(i)]
+                elif self.board[y, x - i] == 1:
+                    pass
+
+        # vertical from top to bottom
+        for i in range(1, 8, 1):
+            if player == Player.USER.value:
+                if self.board[y + i, x] == 0:
+                    break
+                elif self.board[y + i, x] == 1:
+                    self.board[y : y + i, x] = [1 for _ in range(i)]
+                elif self.board[y + i, x] == -1:
+                    pass
+            elif player == Player.AI.value:
+                if self.board[y + i, x] == 0:
+                    break
+                elif self.board[y][x] == -1:
+                    self.board[y : y + 1, x] = [-1 for _ in range(i)]
+                elif self.board[y + i, x] == 1:
+                    pass
+
+        # vertical from bottom to top
+        for i in range(1, 8, 1):
+            if player == Player.USER.value:
+                if self.board[y - i, x] == 0:
+                    break
+                elif self.board[y - i, x] == 1:
+                    self.board[y - i : y, x] = [1 for _ in range(i)]
+                elif self.board[y - i, x] == -1:
+                    pass
+            elif player == Player.AI.value:
+                if self.board[y - i, x] == 0:
+                    break
+                elif self.board[y, x] == -1:
+                    self.board[y - i : y, x] = [-1 for _ in range(i)]
+                elif self.board[y - i, x] == 1:
+                    pass
+
+        # change turn after one player has place
+        self.next_turn()
 
 
 if __name__ == "__main__":
     # For texting purposes
 
     reversi = Reversi()
-    print(reversi.player)
-    reversi.next_turn()
-    print(reversi.player)
+    # print(reversi.player)
+    # reversi.next_turn()
+    # print(reversi.player)
 
+    # print(reversi.board)
+
+    # print(reversi.player)
+    reversi.place(1, (4, 2))
+    reversi.place(0, (5, 4))
+    reversi.place(1, (3, 5))
+    reversi.place(0, (4, 1))
+    reversi.place(1, (6, 4))
+    #   print(reversi.board[4:8, 2])
     print(reversi.board)
