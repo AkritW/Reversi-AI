e865dbfc268a780882a8f6e4db57b0eed5a6721d
AkritW <mail2max.w@gmail.com>
2023-01-06 16:37:40 +0700

    update server file

--- web-gui/server/main.py
+++ web-gui/server/main.py
@@ -6,6 +6,7 @@ from enum import Enum
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import numpy as np
 
 # internal import
 from reversi import Reversi
@@ -14,7 +15,10 @@ from reversi import Reversi
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost", "http://localhost:8080"],
+    allow_origins=[
+        "http://localhost:5173",
+        "localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -33,7 +37,17 @@ async def index() -> HelloWorldMessage:
 
 @app.get("/board")
 async def get_board() -> List[List[int]]:
-    return reversi.board.tolist()
+    board = np.array([[e for e in row] for row in reversi.board])
+    print(board)
+    valid_board = reversi.get_valid_board()
+    print(valid_board)
+    for i, _ in enumerate(board):
+        for j, _ in enumerate(board[i]):
+            if valid_board[i, j] != 0:
+                board[i, j] = 2
+
+    print(board)
+    return board.tolist()
 
 
 class Player(Enum):
@@ -51,4 +65,11 @@ async def place_to_board(
     place_information: PlaceInformation,
 ) -> List[List[int]]:
     reversi.place_inplace(place_information.player, place_information.locs)
-    return reversi.board.tolist()
+    board = [[e for e in row] for row in reversi.board]
+    valid_board = reversi.get_valid_board()
+    for i, _ in enumerate(board):
+        for j, _ in enumerate(board[i]):
+            if valid_board[i, j] != 0:
+                board[i, j] = 2
+
+    return board
